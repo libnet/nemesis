516c0dddee66a9501e18cbd06e442f9bba25dea0
Joachim Nilsson <troglobit@gmail.com>
2018-04-21 13:51:08 +0200

    ip: Missing free() of strdup()'ed string, found by Coverity Scan
    Signed-off-by: Joachim Nilsson <troglobit@gmail.com>

--- src/nemesis-ip.c
+++ src/nemesis-ip.c
@@ -170,6 +170,8 @@ static void ip_cmdline(int argc, char **argv)
 			}
 #else
 			if (strlen(optarg) < 256) {
+				if (device)
+					free(device);
 				device = strdup(optarg);
 				got_link = 1;
 			} else {
@@ -207,6 +209,8 @@ static void ip_cmdline(int argc, char **argv)
 			break;
 		case 'O': /* IP options file */
 			if (strlen(optarg) < 256) {
+				if (ipoptionsfile)
+					free(ipoptionsfile);
 				ipoptionsfile = strdup(optarg);
 				got_ipoptions = 1;
 			} else {
@@ -219,6 +223,8 @@ static void ip_cmdline(int argc, char **argv)
 			break;
 		case 'P': /* payload file */
 			if (strlen(optarg) < 256) {
+				if (payloadfile)
+					free(payloadfile);
 				payloadfile = strdup(optarg);
 				got_payload = 1;
 			} else {
