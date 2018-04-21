52c2aafb9ad909cc25348c6d656db6db849dcda7
Joachim Nilsson <troglobit@gmail.com>
2018-04-21 13:53:21 +0200

    rip: Missing free() of strdup()'ed string, found by Coverity Scan
    Signed-off-by: Joachim Nilsson <troglobit@gmail.com>

--- src/nemesis-rip.c
+++ src/nemesis-rip.c
@@ -241,6 +241,8 @@ static void rip_cmdline(int argc, char **argv)
 			}
 #else
 			if (strlen(optarg) < 256) {
+				if (device)
+					free(device);
 				device = strdup(optarg);
 				got_link = 1;
 			} else {
@@ -299,6 +301,8 @@ static void rip_cmdline(int argc, char **argv)
 			break;
 		case 'O': /* IP options file */
 			if (strlen(optarg) < 256) {
+				if (ipoptionsfile)
+					free(ipoptionsfile);
 				ipoptionsfile = strdup(optarg);
 				got_ipoptions = 1;
 			} else {
@@ -308,6 +312,8 @@ static void rip_cmdline(int argc, char **argv)
 			break;
 		case 'P': /* payload file */
 			if (strlen(optarg) < 256) {
+				if (payloadfile)
+					free(payloadfile);
 				payloadfile = strdup(optarg);
 				got_payload = 1;
 			} else {
