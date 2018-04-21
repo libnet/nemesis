9299db436027d5cb266642e9d9f6700888c191f0
Joachim Nilsson <troglobit@gmail.com>
2018-04-21 13:52:03 +0200

    ospf: Missing free() of strdup()'ed string, found by Coverity Scan
    Signed-off-by: Joachim Nilsson <troglobit@gmail.com>

--- src/nemesis-ospf.c
+++ src/nemesis-ospf.c
@@ -304,6 +304,8 @@ static void ospf_cmdline(int argc, char **argv)
 			}
 #else
 			if (strlen(optarg) < 256) {
+				if (device)
+					free(device);
 				device = strdup(optarg);
 				got_link = 1;
 			} else {
@@ -425,6 +427,8 @@ static void ospf_cmdline(int argc, char **argv)
 			break;
 		case 'O': /* IP options file */
 			if (strlen(optarg) < 256) {
+				if (ipoptionsfile)
+					free(ipoptionsfile);
 				ipoptionsfile = strdup(optarg);
 				got_ipoptions = 1;
 			} else {
@@ -490,6 +494,8 @@ static void ospf_cmdline(int argc, char **argv)
 			break;
 		case 'P': /* payload file */
 			if (strlen(optarg) < 256) {
+				if (payloadfile)
+					free(payloadfile);
 				payloadfile = strdup(optarg);
 				got_payload = 1;
 			} else {
